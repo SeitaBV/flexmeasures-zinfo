da928558914376c01b73468ba4606cbc08c6071d
Nicolas Höning <iam@nicolashoening.de>
2021-09-10 19:51:38 +0200

    introduce ZINFO_SPCID setting (#1)

--- __init__.py
+++ __init__.py
@@ -2,7 +2,7 @@ import os
 import sys
 
 import click
-from flask import Blueprint
+from flask import Blueprint, current_app
 from flask.cli import with_appcontext
 from flexmeasures.data.transactional import task_with_status_report
 import requests
@@ -19,7 +19,6 @@ zinfo_bp = Blueprint("zinfo", __name__)
 
 ZINFO_API_BASE_URL = "https://webservice.z-info.nl/WSR"
 
-
 zinfo_bp.cli.help = "Z-info Data commands"
 
 @zinfo_bp.cli.command("import-sensor-data")
@@ -35,12 +34,18 @@ def import_sensor_data(dryrun: bool = False):
     Import sensor data
     """
     access_token = get_access_token()
-    sensor_id = ""  # TODO: configure tthe sensors we need to read from
+    zinfo_spcid = current_app.config.get("ZINFO_SPCID", None)
+    if not zinfo_spcid:
+        click.echo("ZINFO_SPCID setting is not given!")
+        raise click.Abort
     res = requests.get(
-        f"{ZINFO_API_BASE_URL}/zi_wsr.svc/JSON/NL.13/?spcid=hvcMwd_IR_wk",
+        f"{ZINFO_API_BASE_URL}/zi_wsr.svc/JSON/NL.13/?spcid={zinfo_spcid}",
         headers={"Authorization": access_token},
     )
-    print(res.json())
+    response = res.json()
+    values = response.get('waarden', [])
+    current_app.logger.info(f"Got {len(values)} values...")
+    print(f"Example: {values[0]}")
     # TODO: parse response
     if not dryrun:
         # TODO save
